d052882bdb1d9109a4f580082a4d791b060394b4
Jeff Long <hello@jeffreylong.org>
2014-08-10 17:23:41 -0500

    Bind on selectors; allow arbitrary element #

--- twitchy_v2.js
+++ twitchy_v2.js
@@ -1,15 +1,21 @@
 var Twitchy = {
-  input: function(el, connectTo){
-    el.addEventListener('input', function(ev){
-      var updateEvent, newValue;
-      newValue = ev.currentTarget.value;
-      updateEvent = new CustomEvent('update:' + connectTo, {detail: {value: newValue}});
-      window.dispatchEvent(updateEvent);
-    }, false);
+  input: function(selector, connectTo){
+    var elements = document.querySelectorAll(selector);
+    [].forEach.call(elements, function(el){
+      el.addEventListener('input', function(ev){
+        var updateEvent, newValue;
+        newValue = ev.currentTarget.value;
+        updateEvent = new CustomEvent('update:' + connectTo, {detail: {value: newValue}});
+        window.dispatchEvent(updateEvent);
+      }, false);
+    });
   },
-  output: function(el, connectTo){
-    window.addEventListener('update:' + connectTo, function(ev){
-      el.textContent = ev.detail.value;
-    }, false);
+  output: function(selector, connectTo){
+    var elements = document.querySelectorAll(selector);
+    [].forEach.call(elements, function(el){
+      window.addEventListener('update:' + connectTo, function(ev){
+        el.textContent = ev.detail.value;
+      }, false);
+    });
   }
 };
